b2330907b5bb7a60ba35aa598a3188a0c4351732
Sarankumar-R <saran.sk159@gmail.com>
2020-09-11 19:58:27 +0530

    Updates

---
+++ precache-manifest.14133b1d71f5c3bf291c704140de8ac0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "911c51f5c3d4bd6129d907bc74afe755",
+    "url": "/assessment_netart/index.html"
+  },
+  {
+    "revision": "c1f57969c3c055423636",
+    "url": "/assessment_netart/static/css/main.69484bbf.chunk.css"
+  },
+  {
+    "revision": "b28ee42175719ecdc63b",
+    "url": "/assessment_netart/static/js/2.0f73ca60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/assessment_netart/static/js/2.0f73ca60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1f57969c3c055423636",
+    "url": "/assessment_netart/static/js/main.27900a08.chunk.js"
+  },
+  {
+    "revision": "d7808a1c31c67d8334bd",
+    "url": "/assessment_netart/static/js/runtime-main.f877ab46.js"
+  },
+  {
+    "revision": "29d96123f6fe54ac99593cfc60bbef44",
+    "url": "/assessment_netart/static/media/1.29d96123.png"
+  },
+  {
+    "revision": "57d5c6509602db21e03912c7a89a79e4",
+    "url": "/assessment_netart/static/media/2.57d5c650.png"
+  },
+  {
+    "revision": "b5ddb1417b1786fc22028f4c49e4ffe3",
+    "url": "/assessment_netart/static/media/3.b5ddb141.png"
+  },
+  {
+    "revision": "5762018e4279189fa56715c827648c1c",
+    "url": "/assessment_netart/static/media/logo.5762018e.png"
+  }
+]);
